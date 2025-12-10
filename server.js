db02377f5fd1ce43f7c3ea5025f5c4fde1824b6c
ElizabethBarraza <elibarraza00@outlook.com>
2025-12-10 01:03:27 -0700

    fix: Corregido package.json y script de inicio para CI/CD.

--- server.js
+++ server.js
@@ -1,13 +1,13 @@
 // .js
 const express = require('express');
 const app = express();
-const PORT = 3000; // ¡Asegúrate de que este puerto coincida con tu mapeo de Docker!
+const PORT = 3000; 
 
 app.get('/', (req, res) => {
     res.send('<h1>Control de Gastos funcionando! ¡Contenedor OK!</h1>');
 });
 
-// Esta línea es CRUCIAL para que el servidor se mantenga activo
+
 app.listen(PORT, () => {
     console.log(`Servidor de Control de Gastos corriendo en puerto ${PORT}`);
 });
